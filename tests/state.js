180dbc41fb99300602ef9c4668b31473e0468c52
emrehan <emrehan.tuzun@gmail.com>
2016-05-21 18:12:11 -0700

    Simplify example test configs

--- tests/state.js
+++ tests/state.js
@@ -1,25 +1,14 @@
 exports.config =  {
-      "id": "4f11ca77-9ee2-48cf-0ea8-5038cc185861",
-      "headers": "Authorization: Basic c3lzOmtleQ==\n",
-      "url": "localhost:9090/_state",
-      "pathVariables": {},
-      "preRequestScript": null,
-      "method": "GET",
-      "collectionId": "274d14af-c802-828c-972e-adae5e5b9ce2",
-      "data": null,
-      "dataMode": "params",
-      "name": "name of localhost:9090/v1/types",
-      "description": "",
-      "descriptionFormat": "html",
-      "time": 1463788969505,
-      "version": 2,
-      "responses": [],
-      "currentHelper": "normal",
-      "helperAttributes": {}
+      'name': 'state request',
+      'url': 'localhost:9090/_state',
+      'headers': 'Authorization: Basic c3lzOmtleQ==\n',
+      'method': 'GET',
+      'dataMode': 'params',
+      'id': '4f11ca77-9ee2-48cf-0ea8-5038cc185861'
 }
 
 exports.tests = function () {
-      tests['my'] = JSON.parse(responseBody)['state']['uuid'] == 'uuid';
+      tests['uuid is equal to "uuid"'] = JSON.parse(responseBody)['state']['uuid'] == 'uuid';
       tests['Status code is 200'] = responseCode.code === 200;
       tests['Body matches string'] = responseBody.has('types');
 }
--- tests/types.js
+++ tests/types.js
@@ -1,26 +1,10 @@
 exports.config = {
-      "id": "b35d866f-4f0a-d748-46ae-2dfe4a961d1b",
-      "headers": "Authorization: Basic c3lzOmtleQ==\n",
-      "url": "localhost:9090/v1/types",
-      "pathVariables": {},
-      "preRequestScript": null,
-      "method": "GET",
-      "collectionId": "7e375cc1-c3a7-d546-fde7-7dee761e1789",
-      "data": null,
-      "dataMode": "params",
-      "name": "localhost:9090/v1/types",
-      "description": "types request def\n",
-      "descriptionFormat": "html",
-      "time": 1463869542314,
-      "version": 2,
-      "responses": [],
-      "currentHelper": "basicAuth",
-      "helperAttributes": {
-            "id": "basic",
-            "username": "sys",
-            "password": "key",
-            "saveToRequest": true
-      }
+      'name': 'types request',
+      'url': 'localhost:9090/v1/types',
+      'headers': 'Authorization: Basic c3lzOmtleQ==\n',
+      'method': 'GET',
+      'dataMode': 'params',
+      'id': 'b35d866f-4f0a-d748-46ae-2dfe4a961d1b'
 }
 
 exports.tests = function () {
